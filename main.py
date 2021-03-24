b740b760c20cf4e37b148e0777e033f3723da487
Maradonna <4048848+Maradonna90@users.noreply.github.com>
2021-03-24 17:49:35 +0100

    test print

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ import System.Windows.Forms as WinForms
 
 def main():
     print("Start Main!")
-    loop = tpa.WinformsProactorEventLoop()
-    app_context = WinForms.ApplicationContext()
-    asyncio.set_event_loop(loop)
-    print("Start Forever Loop")
-    loop.run_forever(app_context)
+    # loop = tpa.WinformsProactorEventLoop()
+    # app_context = WinForms.ApplicationContext()
+    # asyncio.set_event_loop(loop)
+    # print("Start Forever Loop")
+    # loop.run_forever(app_context)
 
 
 if __name__ == '__main__':
