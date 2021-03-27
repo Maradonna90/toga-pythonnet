0db035a65414a7aa04600b57861f0ed3410262d9
Maradonna <4048848+Maradonna90@users.noreply.github.com>
2021-03-27 15:48:45 +0100

    fixed command order

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ clr.AddReference("System.Windows.Forms")
 import System.Windows.Forms as WinForms
 from System import Threading
 
+# TODO: Figure out the function call tree for an WindowsApp and recreate.
+# 1. App.__init__
+# 2. self._create_impl()
+# 3. factory.App(interface=self)
+# 4. (Winforms).App.__init__()
+# 5. 
+# TODO: fix the main loop getting stuck
+
 
 class App:
     def __init__(self):
@@ -12,6 +20,8 @@ class App:
 
     def create(self):
         print("CREATE")
+        self.loop = tpa.WinformsProactorEventLoop()
+        asyncio.set_event_loop(self.loop)
         thread = Threading.Thread(Threading.ThreadStart(self.run_app))
         thread.SetApartmentState(Threading.ApartmentState.STA)
         thread.Start()
@@ -20,9 +30,7 @@ class App:
     def run_app(self):
         print("RUN")
         self.native = WinForms.Application
-        self.loop = tpa.WinformsProactorEventLoop()
         self.app_context = WinForms.ApplicationContext()
-        asyncio.set_event_loop(self.loop)
         print("Start Forever Loop")
         self.loop.run_forever(self.app_context)
 
